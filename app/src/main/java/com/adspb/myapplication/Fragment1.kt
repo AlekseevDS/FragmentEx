1a1cec605775074038d703598cc482e977ddec17
AlekseevDS <alekseevdms@gmail.com>
2021-05-29 15:39:08 +0300

    feat: add fragment with transaction text in activity

--- app/src/main/java/com/adspb/myapplication/Fragment1.kt
+++ app/src/main/java/com/adspb/myapplication/Fragment1.kt
@@ -1,59 +1,65 @@
 package com.adspb.myapplication
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
+private var editTextFragment1: EditText? = null
+private var textViewFragment1: TextView? = null
+private var buttonFragment1: Button? = null
+
 class Fragment1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    fun Fragment1() {
+        // Required empty public constructor
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+       val view = inflater.inflate(R.layout.fragment_1, container, false)
+
+        editTextFragment1 = view?.findViewById(R.id.editTextFragment1)
+        textViewFragment1 = view?.findViewById(R.id.textViewFragment1)
+        buttonFragment1 = view?.findViewById(R.id.buttonFragment1)
+
+        buttonFragment1?.setOnClickListener {
+            val S = editTextFragment1?.text.toString()
+            mListener?.onFragment1DataListener(S)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    //В фрагменте определим интерфейс слушателя команд от нашего фрагмента.
+    interface OnFragment1DataListener {
+        fun onFragment1DataListener(string: String?)
     }
+
+    private var mListener: OnFragment1DataListener? = null
+
+// проверим есть ли у вызывающей фрагмент активности наш интерфейс. Если нет, то вызовем RuntimeException,
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mListener = if (context is OnFragment1DataListener) {
+            context
+        } else {
+            throw RuntimeException("$context must implement OnFragment1DataListener")
+        }
+    }
+
+
+
 }
+
+
+
--- app/src/main/java/com/adspb/myapplication/MainActivity.kt
+++ app/src/main/java/com/adspb/myapplication/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.adspb.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), Fragment1.OnFragment1DataListener {
+
+    private var editText: EditText? = null
+    private var textView: TextView? = null
+    private var button: Button? = null
+    private var fragmentContainer: FrameLayout? = null
+    private var fragmentContainer2: FrameLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        fragmentContainer = findViewById(R.id.fragmentContainer)
+        fragmentContainer2 = findViewById(R.id.fragmentContainer2)
+
+/*        button?.setOnClickListener(View.OnClickListener {
+            fun onClick(view: View?) {
+                // TODO some work here
+
+            }
+        })*/
+
+        /*
+        //2й вариант без использования анонимного класса
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                // Do some work here
+            }
+
+        })*/
+
+        //3й Для фрагментов не подходит, только для кнопки в Активити
+        /*fun onMyButtonClick(view: View?) {
+            // выводим сообщение
+            Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
+        }*/
+
+        val fm: FragmentManager = supportFragmentManager
+
+        var fragment: Fragment? = fm.findFragmentById(R.id.fragmentContainer)
+        if (fragment == null) {
+            fragment = Fragment1()
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit()
+        }
+
     }
+
+    override fun onFragment1DataListener(string: String?) {
+        textView?.setText(string);
+    }
+
 }
+
+
